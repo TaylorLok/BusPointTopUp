6f3185bbcc8aabc9eb00b4a6d056781f0615422f
ChairmanT <taylor@dwebserver.net>
2023-08-19 01:29:12 +0200

    initial commit of the java spring project

---
+++ src/main/java/com/buspointtopup/org/BusPointTopUpApplication.java
@@ -0,0 +1,13 @@
+package com.buspointtopup.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusPointTopUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusPointTopUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buspointtopup/org/BusPointTopUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buspointtopup.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusPointTopUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
